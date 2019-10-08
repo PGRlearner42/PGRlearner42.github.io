d74ece3baeac79d5b1f729170dd0d5565d6ac871
PGRlearner42 <45729067+PGRlearner42@users.noreply.github.com>
2019-10-07 22:46:51 -0400

    Better overall javascript code
    Also has code for the score board. Smoother javascript code

--- colorSlots.js
+++ colorSlots.js
@@ -1,30 +1,32 @@
-function inputNum(){
-	var color = ["red","orange","yellow","green","blue","purple"];
-	var rand = color[Math.floor(Math.random()*6)]
-	var randAlpha = color[Math.floor(Math.random()*6)]
-	var randBeta = color[Math.floor(Math.random()*6)]
-	var randCharlie = color[Math.floor(Math.random()*6)]
-
-	const one = document.getElementById("one").style.backgroundColor = rand;
-	const two = document.getElementById("two").style.backgroundColor = randAlpha;
-	const three = document.getElementById("three").style.backgroundColor = randBeta;
-	const four = document.getElementById("four").style.backgroundColor = randCharlie;
-
-	if(rand == color[0] && randAlpha == color[0] && randBeta == color[0] && randCharlie == color[0]){
-		alert("you won!")
-	} else if(rand == color[1] && randAlpha == color[1] && randBeta == color[1] && randCharlie == color[31]){
-		alert("you won!")	
-	}else if(rand == color[2] && randAlpha == color[2] && randBeta == color[2] && randCharlie == color[2]){
-		alert("you won!")	
-	}else if(rand == color[3] && randAlpha == color[3] && randBeta == color[3] && randCharlie == color[3]){
-		alert("you won!")	
-	}else if(rand == color[4] && randAlpha == color[4] && randBeta == color[4] && randCharlie == color[4]){
-		alert("you won!")	
-	}else if(rand == color[5] && randAlpha == color[5] && randBeta == color[5] && randCharlie == color[5]){
-		alert("you won!")	
-	}else if(rand == color[6] && randAlpha == color[6] && randBeta == color[6] && randCharlie == color[6]){
-		alert("you won!")	
-	}else {
-		alert("you lose!")
+let color = document.querySelectorAll('[data-color]');
+let button=document.getElementById('button')
+let positive=document.getElementById('positive');
+let negative=document.getElementById('negative');
+let difference=document.getElementById('difference')
+	let num=0;
+	let num2=0;
+	function increment(){
+		num2++	
+		return num2
 	}
+	function decrement(){
+		num++
+		return num
+	}
+function input(){
+		let inputColor=['red','green','blue'];
+		const alpha=inputColor[Math.floor(Math.random()*inputColor.length)];
+		const beta=inputColor[Math.floor(Math.random()*inputColor.length)];
+		const gamma=inputColor[Math.floor(Math.random()*inputColor.length)];
+		const delta=inputColor[Math.floor(Math.random()*inputColor.length)];
+		const rand=color[0].style.backgroundColor=alpha;
+		const rand2=color[1].style.backgroundColor=beta;
+		const rand3=color[2].style.backgroundColor=gamma;
+		const rand4=color[3].style.backgroundColor=delta;
+		if(rand==rand2&&rand2==rand3&&rand3==rand4){
+			positive.innerHTML=increment()
+		}else{
+			negative.innerHTML=decrement()
+		}
 }
+button.addEventListener('click',()=> input())
